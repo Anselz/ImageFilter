789b99420fd230b3433d4b6d7fc202650fd2ca83
Anselz <Janselz@gmail.com>
2013-12-26 12:17:10 +0800

    Upload Source Codes.

---
+++ Image/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Image
+//
+//  Created by Anselz on 13-12-24.
+//  Copyright (c) 2013年 Anselz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
